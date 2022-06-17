8b37fbba53d9d6a2b1a2c2b136dda521111d6af7
Dylan Lewis <dylanl826@gmail.com>
2022-06-17 18:33:07 -0500

    Added model to track purchases.

--- sd_app/models.py
+++ sd_app/models.py
@@ -1,2 +1,17 @@
 from django.db import models
+from django.conf import settings
 
+class Transaction(models.Model):
+    """
+    Transaction model
+    """
+    # Transaction ID
+    transaction_id = models.AutoField(primary_key=True)
+    # Amount purchased
+    amount = models.FloatField()
+    # Location where transaction occurred, true if in state, false if out of state.
+    location = models.BooleanField()
+    # Date and time of transaction
+    date = models.DateTimeField()
+    # User who made the transaction
+    customer_id = models.ForeignKey(settings.AUTH_USER_MODEL)
