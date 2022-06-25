5eb6a9247f1bbeefab641d759c84e3d5ae9b1009
KennyZhang12138 <97780120+KennyZhang12138@users.noreply.github.com>
2022-06-25 05:44:06 -0500

    Update urls.py

--- sd_app/urls.py
+++ sd_app/urls.py
@@ -1,9 +1,10 @@
 # URL mappings for the application
 from django.urls import path
-from .views import LoginPageView, HomePageView
+from .views import LoginPageView, HomePageView, ProfilePageView, HistoryPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('login/', LoginPageView.as_view(), name='login'),
     path('profile/', ProfilePageView.as_view(), name = 'profile'),
+    path('history/', HistoryPageView.as_view(), name = 'history'),
     ]
