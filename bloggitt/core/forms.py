7ac2daf74d4f2da3a8279adbf2ddde15dd0a61a3
John Jackson <jdreaperjackson@gmail.com>
2022-06-26 19:31:22 -0500

    swd_v2

---
+++ bloggitt/core/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Comment, Profile, Post
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2', 'email')
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'username',  
+            'email', 
+        ]
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [
+            'full_name',
+            'address_1',
+            'address_2',
+            'city',
+            'state',
+            'zipcode',
+        ]
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'body')
+        
+        widgets={
+
+            'name':forms.TextInput(attrs={'class' : 'form-control'}),
+            'body':forms.Textarea(attrs={'class' : 'form-control'})
+
+        }
