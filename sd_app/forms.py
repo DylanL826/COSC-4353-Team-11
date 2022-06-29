11b11e836f9c74a0fa3d7ae205acd4369209d61d
Kenny Zhang <dzhang33@CougarNet.UH.EDU>
2022-06-28 20:54:36 -0500

    add a comment of price model to views.py

--- sd_app/forms.py
+++ sd_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-from sd_app.models import UserProfile, Transaction
+from models import UserProfile, Transaction
 
 
 
