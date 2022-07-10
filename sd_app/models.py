e4b60f507e07c1bb771da62cf15eb6ca8c5b5031
John Jackson <jdreaperjackson@gmail.com>
2022-07-10 10:03:08 -0500

    Changed user profile model to match assignment requirements

--- sd_app/models.py
+++ sd_app/models.py
@@ -22,13 +22,67 @@ class Transaction(models.Model):
     def __str__(self):
         return "Amount: " + str(self.amount) + " gal. Location: at " + str(self.location) + ". Delivery Date: " + str(self.delivery_date)
 
+STATE_CHOICES = (
+    ('al','AL'),
+    ('ak','AK'),
+    ('az','AZ'),
+    ('ar','AR'),
+    ('ca','CA'),
+    ('co','CO'),
+    ('ct','CT'),
+    ('de','DE'),
+    ('fl','FL'),
+    ('ga','GA'),
+    ('hi','HI'),
+    ('id','ID'),
+    ('il','IL'),
+    ('in','IN'),
+    ('ia','IA'),
+    ('ks','KS'),
+    ('ky','KY'),
+    ('la','LA'),
+    ('me','ME'),
+    ('md','MD'),
+    ('ma','MA'),
+    ('mi','MI'),
+    ('mn','MN'),
+    ('ms','MS'),
+    ('mo','MO'),
+    ('mt','MT'),
+    ('ne','NE'),
+    ('nv','NV'),
+    ('nh','NH'),
+    ('nj','NJ'),
+    ('nm','NM'),
+    ('ny','NY'),
+    ('nc','NC'),
+    ('nd','ND'),
+    ('oh','OH'),
+    ('ok','OK'),
+    ('or','OR'),
+    ('pa','PA'),
+    ('ri','RI'),
+    ('sc','SC'),
+    ('sd','SD'),
+    ('tn','TN'),
+    ('tx','TX'),
+    ('ut','UT'),
+    ('vt','VT'),
+    ('va','VA'),
+    ('wa','WA'),
+    ('wv','WV'),
+    ('wi','WI'),
+    ('wy','WY'),
+)
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=100, blank=True)
-    address = models.CharField(max_length=100, blank=True)
+    full_name = models.CharField(max_length=50, blank=True)
+    address_line_1 = models.CharField(max_length=100, blank=True)
+    address_line_2 = models.CharField(max_length=100, blank=True)
     city = models.CharField(max_length=100, blank=True)
-    state = models.CharField(max_length=100, blank=True)
-    zip_code = models.CharField(max_length=100, blank=True)
+    state = models.CharField(max_length=2, choices=STATE_CHOICES, blank=True)
+    zip_code = models.CharField(max_length=9, blank=True)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
