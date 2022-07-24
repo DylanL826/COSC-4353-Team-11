30e7cbba331d8ccc73f81ef5a55d3e2923448fca
DylanL826 <dylanl826@gmail.com>
2022-07-24 09:55:34 -0500

    Switched from purchaseHistory template to History template after issue displaying values.

--- sd_app/urls.py
+++ sd_app/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('profile/', views.profilePage, name = 'profile'),
     path('buy/', views.buyPage, name='buy'),
     path("register/", views.registerPage, name="register"),
-    path('purchase_history/', views.purchaseHistoryPage, name='purchase_history'),
+    path('history/', views.historyPage, name='history'),
 
     ]
