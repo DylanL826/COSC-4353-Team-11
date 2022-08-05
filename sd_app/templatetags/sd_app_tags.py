6adb0117551112c9a3a1f271e793d6a0f7c61933
KennyZhang12138 <97780120+KennyZhang12138@users.noreply.github.com>
2022-08-05 13:07:48 -0500

    Delete sd_app/templatetags directory
    delete github merge code error

--- sd_app/templatetags/sd_app_tags.py
+++
@@ -1,6 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.simple_tag
-def calculate_price(gallons_requested, suggested_price):
-    return gallons_requested * suggested_price
